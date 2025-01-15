837690c05e3ac06abf0a45075556e9d3fb0ad714
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-01-15 16:53:54 +0300

    Update telegram.ts

--- api/telegram.ts
+++ api/telegram.ts
@@ -1,6 +1,6 @@
 // api/telegram.ts
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import crypto from 'crypto'; // Изменено для совместимости с ES-модулями
+import crypto from 'crypto';
 
 const BOT_TOKEN = import.meta.env.VITE_TOKEN;
 
@@ -8,14 +8,14 @@ function checkTelegramAuth(data: any): boolean {
     const { hash, ...rest } = data;
     const checkString = Object.keys(rest)
         .sort()
-        .map((key) => `${key}=${rest[key]}`)
+        .map(key => `${key}=${rest[key]}`)
         .join('\n');
     const secretKey = crypto.createHash('sha256').update(BOT_TOKEN).digest();
     const hmac = crypto.createHmac('sha256', secretKey).update(checkString).digest('hex');
     return hmac === hash;
 }
 
-const handler = (req: VercelRequest, res: VercelResponse) => {
+export default async (req: VercelRequest, res: VercelResponse) => {
     const authData = req.query;
 
     if (checkTelegramAuth(authData)) {
@@ -24,5 +24,3 @@ const handler = (req: VercelRequest, res: VercelResponse) => {
         return res.status(403).json({ status: 'error', message: 'Invalid authentication' });
     }
 };
-
-export default handler; // Используем export default вместо module.exports
