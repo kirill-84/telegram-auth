7b064f8f669f928d7aa0086c26deccc75359ab1a
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-01-15 16:15:44 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -13,7 +13,7 @@ interface TelegramUser {
 }
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const [user, setUser] = useState<TelegramUser | null>(null);
 
     const handleAuthSuccess = (userData: TelegramUser) => {
