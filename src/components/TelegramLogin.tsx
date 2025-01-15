d32216eae2668f518fe9c73c48f3240cfc80971d
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-01-15 15:45:08 +0300

    Update TelegramLogin.tsx

--- src/components/TelegramLogin.tsx
+++ src/components/TelegramLogin.tsx
@@ -1,20 +1,19 @@
 // src/components/TelegramLogin.tsx
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const TelegramLogin: React.FC = () => {
-    return (
-        <div>
-            <script
-                async
-                src="https://telegram.org/js/telegram-widget.js?22"
-                data-telegram-login="TmaAuthBot"
-                data-size="large"
-                data-auth-url="/api/telegram"
-                data-request-access="write"
-            ></script>
-        </div>
-    )
-};
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://telegram.org/js/telegram-widget.js?22';
+        script.async = true;
+        script.setAttribute('data-telegram-login', 'TmaAuthBot');
+        script.setAttribute('data-size', 'large');
+        script.setAttribute('data-auth-url', '/api/telegram');
+        script.setAttribute('data-request-access', 'write');
+        document.getElementById('telegram-login-container')?.appendChild(script);
+    }, []);
 
+    return <div id="telegram-login-container"></div>;
+};
 
 export default TelegramLogin;
