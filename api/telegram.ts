26529ada33534bcc3f2cdecb50ab7500ee86caea
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-01-16 00:33:29 +0300

    Update telegram.ts

--- api/telegram.ts
+++ api/telegram.ts
@@ -8,7 +8,7 @@ function checkTelegramAuth(data: any): boolean {
         .sort()
         .map((key) => `${key}=${rest[key]}`)
         .join('\n');
-    const secretKey = crypto.createHash('sha256').update(process.env.VITE_TOKEN).digest();
+    const secretKey = crypto.createHash('sha256').update('8091835662:AAEKAnpUhu03V8zz8quh6qBVs5Pu4wiUwQw').digest();
     const hmac = crypto.createHmac('sha256', secretKey).update(checkString).digest('hex');
     return hmac === hash;
 }
