cc6c446c9fdc8dea176fe0408471ff090e910a43
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-01-15 15:47:15 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000, //specify the port number here
     proxy: {
       '/api': {
-        target: 'https://tg-auth-six.vercel.app', // Vercel will handle this in production
+        target: 'http://localhost:3000', // Vercel will handle this in production
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
