8e1a919d78c0253fd7ac9c8889b3818f184fbacb
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-01-17 14:54:10 +0300

    Update telegram.cjs

--- api/telegram.cjs
+++ api/telegram.cjs
@@ -2,13 +2,19 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import crypto from 'crypto';
 
+const BOT_TOKEN = process.env.VITE_TOKEN;
+
+if (!BOT_TOKEN) {
+    throw new Exception("BOT_TOKEN is not defined")
+}
+
 function checkTelegramAuth(data: any): boolean {
     const { hash, ...rest } = data;
     const checkString = Object.keys(rest)
         .sort()
         .map((key) => `${key}=${rest[key]}`)
         .join('\n');
-    const secretKey = crypto.createHash('sha256').update(import.meta.env.VITE_TOKEN ?? "").digest();
+    const secretKey = crypto.createHash('sha256').update(BOT_TOKEN).digest();
     const hmac = crypto.createHmac('sha256', secretKey).update(checkString).digest('hex');
     return hmac === hash;
 }
