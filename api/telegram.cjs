758cc95eea254d5da1f733d35c6a830a0acaecc2
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-01-17 14:57:18 +0300

    Update telegram.cjs

--- api/telegram.cjs
+++ api/telegram.cjs
@@ -2,7 +2,7 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import crypto from 'crypto';
 
-const BOT_TOKEN = process.env.VITE_TOKEN;
+var BOT_TOKEN = process.env.VITE_TOKEN;
 
 if (!BOT_TOKEN) {
     throw new Exception("BOT_TOKEN is not defined")
