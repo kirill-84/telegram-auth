e62bfa00bcc92e065610410f0c0571accb366c98
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-01-21 20:27:12 +0300

    Update telegram.tsx

--- api/telegram.tsx
+++ api/telegram.tsx
@@ -40,25 +40,20 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
       .map((key) => `${key}=${authData[key]}`) // Формат key=value
       .join("\n");*/
 
-    const processAuthData = (authData: Map<string, any>): string => {
-  // Получение ключей и их сортировка
-  const keys = Array.from(authData.keys()).sort();
+    const keys = Array.from(authData.keys()).sort();
 
-  // Формирование массива строк "key=value"
-  const dataCheckStrings: string[] = keys.map((key) => {
-    let value = authData.get(key);
-
-    if (typeof value === 'object') {
-      value = JSON.stringify(value); // Преобразуем объекты в строку
-    }
-
-    return ${key}=${value};
-  });
-
-  // Объединяем строки с разделителем '\n'
-  return dataCheckStrings.join('\n');
-};
-    const dataCheckString = dataCheckStrings;
+    const dataCheckStrings: string[] = [];
+    keys.forEach((key) => {
+      let value = authData[key];
+    
+      if (typeof value === 'object') {
+        value = JSON.stringify(value); // Преобразуем объект в строку
+      }
+    
+      dataCheckStrings.push(`${key}=${value}`);
+    });
+    
+    const dataCheckString = dataCheckStrings.join('\n');
 
     console.log("Data Check String:", dataCheckString);
 
