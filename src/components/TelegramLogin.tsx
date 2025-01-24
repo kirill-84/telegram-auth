24a4a4cbfd3d186ed5e382bdd5f97aec803a9f69
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-01-24 14:10:36 +0300

    Update TelegramLogin.tsx

--- src/components/TelegramLogin.tsx
+++ src/components/TelegramLogin.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 
+
 interface TelegramLoginProps {
   onAuth: (userData: any) => void; // Callback to pass user data to the parent
   onError: (error: string) => void; // Callback to handle errors
