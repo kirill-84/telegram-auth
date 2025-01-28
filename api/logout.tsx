158d7a8944e0853b74198f939b34f4c5edf5e27f
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-01-28 16:35:32 +0300

    Update logout.tsx

--- api/logout.tsx
+++ api/logout.tsx
@@ -1,7 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  
   res.setHeader('Set-Cookie', 'session=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT');
   return res.status(200).json({ success: true });
 }
