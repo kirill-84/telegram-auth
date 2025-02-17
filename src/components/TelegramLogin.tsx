0f89b494ff65f221f7a2b5da0d5f7987eb7e279e
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-02-17 22:01:50 +0300

    Update TelegramLogin.tsx

--- src/components/TelegramLogin.tsx
+++ src/components/TelegramLogin.tsx
@@ -11,7 +11,7 @@ const TelegramLogin: React.FC<TelegramLoginProps> = ({ onAuth, onError }) => {
     const BOT_NAME = process.env.BOT_NAME as string;
     script.src = 'https://telegram.org/js/telegram-widget.js?22';
     script.async = true;
-    script.setAttribute('data-telegram-login', BOT_NAME); // Replace with your bot username
+    script.setAttribute('data-telegram-login', "BOT_NAME"); // Replace with your bot username
     script.setAttribute('data-size', 'large');
     script.setAttribute('data-auth-url', '/api/telegram'); // Endpoint to handle authentication
     script.setAttribute('data-request-access', 'write');
